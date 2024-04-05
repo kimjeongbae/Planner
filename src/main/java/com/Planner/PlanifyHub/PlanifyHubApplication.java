dc03eb972acaf26f32d7fdaa3cad69554d3c88c5
kimjeongbae <gprhf651@naver.com>
2024-04-05 16:21:40 +0900

    test

---
+++ src/main/java/com/Planner/PlanifyHub/PlanifyHubApplication.java
@@ -0,0 +1,13 @@
+package com.Planner.PlanifyHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanifyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanifyHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Planner/PlanifyHub/PlanifyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Planner.PlanifyHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanifyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
